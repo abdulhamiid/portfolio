a48680fe2417926437cc02aef971e88f35044b3a
abdulhamiid <adioabdulhamid@yahoo.com>
2022-05-09 22:22:29 +0100

    Fix eslintrc.json errors

--- js/index.js
+++ js/index.js
@@ -1,29 +1,29 @@
-let menu = document.querySelector(".menu");
-let logo= document.querySelector("header h3");
-let hamburger = document.querySelector('#openmenu');
-let closeHamburger = document.querySelector('#closemenu');
-let ul= document.querySelector('.nav-links');
-let navLinks = document.querySelectorAll('.nav-links li');
-let menuBg = document.querySelector('.menu-bg')
-let lis = Array.from(navLinks);
+const menu = document.querySelector('.menu');
+const logo = document.querySelector('header h3');
+const hamburger = document.querySelector('#openmenu');
+const closeHamburger = document.querySelector('#closemenu');
+const ul = document.querySelector('.nav-links');
+const navLinks = document.querySelectorAll('.nav-links li');
+const menuBg = document.querySelector('.menu-bg');
+const lis = Array.from(navLinks);
 
-menu.addEventListener("click", function() {
-  toggler();
-  hamburger.classList.toggle('remove');
-  closeHamburger.classList.toggle('remove');  
-})
-
-for (let i = 0; i < lis.length; i++) {
-  lis[i].addEventListener('click', function() {
-    toggler()
-    closeHamburger.classList.add('remove');
-    hamburger.classList.remove('remove');
-  })
+function toggler() {
+  logo.classList.toggle('hide');
+  ul.classList.toggle('hide');
+  menuBg.classList.toggle('remove');
+  document.body.classList.toggle('no-scroll');
 }
 
-function toggler () {
-  logo.classList.toggle('hide');  
-  ul.classList.toggle('hide');
-  menuBg.classList.toggle('remove')
-  document.body.classList.toggle("no-scroll")
+menu.addEventListener('click', () => {
+  toggler();
+  hamburger.classList.toggle('remove');
+  closeHamburger.classList.toggle('remove');
+});
+
+for (let i = 0; i < lis.length; i += 1) {
+  lis[i].addEventListener('click', () => {
+    toggler();
+    closeHamburger.classList.add('remove');
+    hamburger.classList.remove('remove');
+  });
 }
