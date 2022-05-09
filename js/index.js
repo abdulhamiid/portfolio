dad4c028bb4a156cd0159c1e9cef03cc424539b0
Christabelle143 <christabelleyouta143@gmail.com>
2022-05-09 18:16:40 +0100

    Add functionality to the mobile nav links

--- js/index.js
+++ js/index.js
@@ -3,10 +3,18 @@ let logo= document.querySelector("header h3");
 let hamburger = document.querySelector('#openmenu');
 let closeHamburger = document.querySelector('#closemenu');
 let links= document.querySelector(".nav-links");
+let navLinks = document.querySelectorAll(".nav-links li");
+let lis = Array.from(navLinks);
 
 menu.addEventListener("click", function(e){
   logo.classList.toggle("hide");  
   links.classList.toggle("hide");
   hamburger.classList.toggle('remove');
   closeHamburger.classList.toggle('remove')
+  document.body.classList.toggle("no-scroll")
 })
+for (let i = 0; i<lis.length; i++){
+  lis[i].addEventListener("click", function(e){
+    links.classList.add("remove");
+  })
+}
