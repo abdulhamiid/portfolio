c4cc12357a610bafb30d91dbd54f775ed925413e
abdulhamiid <adioabdulhamid@yahoo.com>
2023-02-10 15:49:57 +0100

    Add swipe bundle files

--- javascript/index.js
+++ javascript/index.js
@@ -180,3 +180,25 @@ inputs.forEach((item) => {
     localStorage.setItem('portfolio', data);
   });
 });
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
